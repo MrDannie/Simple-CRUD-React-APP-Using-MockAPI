c4e6a7619b3469ac981b97ad66c3fe527db0f10d
Blockchain Developer <blockchaindeveloper@blockchaindev.local>
2023-11-23 15:49:52 +0100

    here we go

---
+++ src/herewego.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const herewego = (props) => {
+  return <div>herewego</div>;
+};
+
+herewego.propTypes = {};
+
+export default herewego;
